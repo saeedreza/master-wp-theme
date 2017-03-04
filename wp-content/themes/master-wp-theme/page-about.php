66e367e32b04944783867ff1983ca7e4d55ba986
Saeed Abbaspour <arashrj@gmail.com>
2017-03-04 22:09:53 +0330

    Adds the about page

---
+++ wp-content/themes/master-wp-theme/page-about.php
@@ -0,0 +1,25 @@
+<?php 
+get_header();  
+if(have_posts()): the_post();
+?>
+	<div id="page-about" class="content">  
+		<div id="inner-content" class="row">  
+
+		    <main id="main" class="large-12 medium-12 columns" role="main"> 
+				 <article id="post-<?php the_ID(); ?>" role="article" itemscope itemtype="http://schema.org/WebPage">
+					<header class="article-header">
+						<h1 class="page-title"><?php the_title(); ?></h1>
+					</header> 
+					
+					<section class="entry-content" itemprop="articleBody">
+						<?php the_content(); ?>
+					</section> <!-- end about section --> 							
+				</article> <!-- end about -->
+			</main> 
+		    
+		</div> <!-- end #inner-content -->
+	</div> <!-- end #content -->
+<?php
+endif; 
+get_footer(); 
+?>
