b971097a2fb217a96d4476ee2eed45eac70b9f9f
Arash Jafari <arashrj@gmail.com>
2017-03-06 02:31:11 +0330

    Adds archive page of blog posts

---
+++ wp-content/themes/master-wp-theme/page-blog.php
@@ -0,0 +1,41 @@
+<?php 
+	get_header(); 
+
+//---------------------
+$post_args = array( 
+	'posts_per_page' => -1
+);
+$post_query = new WP_Query($post_args);
+//---------------------
+?>
+			
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+	
+		    <main id="main" class="large-8 medium-8 columns" role="main">
+		    
+			    <?php if ($post_query->have_posts()) : while ($post_query->have_posts()) : $post_query->the_post(); ?>
+			 
+					<!-- To see additional archive styles, visit the /parts directory -->
+					<?php get_template_part( 'parts/loop', 'archive' ); ?>
+				    
+				<?php endwhile; ?>	
+
+					<?php joints_page_navi(); ?>
+					
+				<?php else : ?>
+											
+					<?php get_template_part( 'parts/content', 'missing' ); ?>
+						
+				<?php endif; ?>
+																								
+		    </main> <!-- end #main -->
+		    
+		    <?php get_sidebar(); ?>
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
