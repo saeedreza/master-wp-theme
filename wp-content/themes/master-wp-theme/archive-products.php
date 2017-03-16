df5cdd54def700a40eab762e123707ba87f79287
Saeed Abbaspour <arashrj@gmail.com>
2017-03-17 01:23:34 +0330

    Show products separated by their categories

--- wp-content/themes/master-wp-theme/archive-products.php
+++ wp-content/themes/master-wp-theme/archive-products.php
@@ -1,19 +1,45 @@
-<?php get_header(); ?>
+<?php 
+	get_header(); 
+
+//---------------------
+$categories = get_terms( 'product_cat', array('hide_empty' => false) );
+ 
+function getProductsByCategory($category){
+	$post_args = array( 
+		'post_type' => 'products', 
+		'product_cat' => $category,
+		'posts_per_page' => -1
+	);
+	$wp_query = new WP_Query($post_args);
+	return $wp_query;
+}
+//---------------------
+?>
 			
 	<div id="content">
 	
 		<div id="inner-content" class="row">
 	
-		    <main id="main" class="large-12 medium-12 columns" role="main">
-    
-			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		    <main id="main" class="large-12 medium-12 columns" role="main"> 
+			    <?php 
+					if ($categories) : 
+						foreach($categories as $category ):
+				?>
+						<h2><?php echo $category->name; ?></h2>
+						<hr /> 
+				<?php
+						$wp_query = getProductsByCategory($category->slug); 
+						
+					  	while ($wp_query->have_posts()) :
+					  	$wp_query->the_post(); 
+				?>
 			  
 					<?php get_template_part( 'parts/loop', 'product' ); ?>
 				    
 				<?php endwhile; ?>	
 
-					<?php master_page_navi(); ?>
-					
+				<?php endforeach; ?>	
+ 
 				<?php else : ?>
 											
 					<?php get_template_part( 'parts/content', 'missing' ); ?>
--- wp-content/themes/master-wp-theme/parts/loop-product.php
+++ wp-content/themes/master-wp-theme/parts/loop-product.php
@@ -1,6 +1,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
 	<header class="article-header">
-		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 	</header> <!-- end article header -->
 					
 	<section class="entry-content" itemprop="articleBody">
@@ -8,7 +8,7 @@
 			$description = get_field('description'); 
 			$image = get_field('image'); 
 		?> 
-		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['caption']; ?>" />
+		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['caption']; ?>" width="150" />
 		<div><?php echo $description; ?></div> 
 	</section> <!-- end article section -->
 						
