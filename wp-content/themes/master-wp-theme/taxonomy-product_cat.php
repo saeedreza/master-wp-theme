bb0a2f5e8598ece44fa08b7413e5d8b6ca73c65b
Saeed Abbaspour <arashrj@gmail.com>
2017-03-19 22:52:43 +0430

    Merge taxon-product_cat page sections into a file

--- wp-content/themes/master-wp-theme/taxonomy-product_cat.php
+++ wp-content/themes/master-wp-theme/taxonomy-product_cat.php
@@ -13,7 +13,21 @@
 		
 		    	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			 
-					<?php get_template_part( 'parts/loop', 'category_product' ); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
+						<header class="article-header">
+							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						
+						</header> <!-- end article header -->
+										
+						<section class="entry-content" itemprop="articleBody">
+							<img src="<?php the_post_thumbnail_url(); ?>" />
+							<?php the_content(); ?>
+						</section> <!-- end article section -->
+											
+						<footer class="article-footer">
+							<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'mastertheme') . '</span> ', ', ', ''); ?></p>
+						</footer> <!-- end article footer -->				    						
+					</article> <!-- end article -->
 				    
 				<?php endwhile; ?>	
 
@@ -21,7 +35,12 @@
 					
 				<?php else : ?>
 											
-					<?php get_template_part( 'parts/content', 'missing' ); ?>
+					<header class="article-header">
+						<h1>Post Not Found!</h1>
+					</header>
+					<section class="entry-content">
+						<p>Uh Oh. Something is missing. Try double checking things.</p>
+					</section>
 						
 				<?php endif; ?>
 		
