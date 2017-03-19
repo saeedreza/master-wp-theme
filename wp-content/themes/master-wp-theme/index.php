5f305e3ccb919c5854596d397abd8632e02dd298
Saeed Abbaspour <arashrj@gmail.com>
2017-03-19 22:40:03 +0430

    Merge index page sections into a file

--- wp-content/themes/master-wp-theme/index.php
+++ wp-content/themes/master-wp-theme/index.php
@@ -8,16 +8,34 @@
 		    
 			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			 
-					<!-- To see additional archive styles, visit the /parts directory -->
-					<?php get_template_part( 'parts/loop', 'blog' ); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
+						<header class="article-header">
+							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						
+						</header> <!-- end article header -->
+										
+						<section class="entry-content" itemprop="articleBody">
+							<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
+							<?php the_content('<button class="tiny">' . __( 'Read more...', 'masterwp' ) . '</button>'); ?>
+						</section> <!-- end article section -->
+											
+						<footer class="article-footer">
+							<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
+						</footer> <!-- end article footer -->				    						
+					</article> <!-- end article -->
 				    
 				<?php endwhile; ?>	
 
 					<?php master_page_navi(); ?>
 					
 				<?php else : ?>
-											
-					<?php get_template_part( 'parts/content', 'missing' ); ?>
+
+					<header class="article-header">
+						<h1>Post Not Found!</h1>
+					</header>
+					<section class="entry-content">
+						<p>Uh Oh. Something is missing. Try double checking things.</p>
+					</section>
 						
 				<?php endif; ?>
 																								
