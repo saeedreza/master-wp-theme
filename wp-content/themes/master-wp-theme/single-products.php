5caa8ab6c944d51b76aabd18525ab3ad428f9d99
Saeed Abbaspour <arashrj@gmail.com>
2017-03-19 22:54:40 +0430

    Merge single-products page sections into a file

--- wp-content/themes/master-wp-theme/single-products.php
+++ wp-content/themes/master-wp-theme/single-products.php
@@ -13,11 +13,31 @@ $blog_posts = getPostsByCategory($product_post_category->slug,3);
 		
 		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
  
-		    	<?php get_template_part( 'parts/loop', 'single_product' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+										
+					<header class="article-header">	
+						<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
+					</header> <!-- end article header -->
+									
+					<section class="entry-content" itemprop="articleBody">
+						<img src="<?php the_post_thumbnail_url(); ?>" />
+						<?php the_content(); ?>
+					</section> <!-- end article section -->
+										
+					<footer class="article-footer">
+						
+					</footer> <!-- end article footer -->
+											
+				</article> <!-- end article -->				
 		    					
 		    <?php endwhile; else : ?>
 		
-		   		<?php get_template_part( 'parts/content', 'missing' ); ?>
+				<header class="article-header">
+					<h1>Post Not Found!</h1>
+				</header>
+				<section class="entry-content">
+					<p>Uh Oh. Something is missing. Try double checking things.</p>
+				</section>
 
 		    <?php endif; ?>
 
