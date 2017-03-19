06b4f6ec2099f3ebcce724a395baf4a809c452a1
Saeed Abbaspour <arashrj@gmail.com>
2017-03-19 22:04:34 +0430

    Adds related blog posts

--- wp-content/themes/master-wp-theme/assets/functions/custom-functions.php
+++ wp-content/themes/master-wp-theme/assets/functions/custom-functions.php
@@ -11,3 +11,13 @@ function getProductsByCategory($category , $count = -1){
 	return $wp_query;
 }
 
+// Get posts by category
+function getPostsByCategory($category , $count = -1){
+	$post_args = array( 
+        'post_type'   => 'post',
+        'category_name' => $category,
+		'posts_per_page' => $count
+	);
+	$wp_query = new WP_Query($post_args);
+	return $wp_query;
+}
--- wp-content/themes/master-wp-theme/single-products.php
+++ wp-content/themes/master-wp-theme/single-products.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php 
+get_header();
+ 
+$product_post_category = get_category(get_field('post_category')); 
+$blog_posts = getPostsByCategory($product_post_category->slug,3);
+
+?>
 			
 <div id="content">
 	<div id="inner-content" class="row">
@@ -6,7 +12,7 @@
 		<main id="main" class="large-12 medium-12 columns first" role="main">
 		
 		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
+ 
 		    	<?php get_template_part( 'parts/loop', 'single_product' ); ?>
 		    					
 		    <?php endwhile; else : ?>
@@ -16,6 +22,19 @@
 		    <?php endif; ?>
 
 		</main> <!-- end #main -->
+
+
+		<div>
+			<ul>
+			<?php if ($blog_posts->have_posts()) : while ($blog_posts->have_posts()) : $blog_posts->the_post(); ?>
+
+				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+								
+			<?php endwhile; ?>
+		 
+			<?php endif; ?>
+			</ul>
+		</div>
  
 	</div> <!-- end #inner-content -->
 
